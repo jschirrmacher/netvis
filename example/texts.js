25039ec7d21cf70f1dd2e57d35c3d681971bb654
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-10-03 12:04:55 +0200

    Fix for #6: Added a default content in the example

--- example/texts.js
+++ example/texts.js
@@ -4,12 +4,14 @@ const allTexts = {
   "de": {
     "topics": "Themen",
     "interestedParties": "Interessierte Personen",
-    "linkTitle": "{{title}} anzeigen"
+    "linkTitle": "{{title}} anzeigen",
+    "defaultDescription": "Es gibt aktuell noch keine Beschreibung zu diesem Eintrag. Informationen könntest du in [GitHub](https://github.com/jschirrmacher/netvis/blob/master/example/data.json) ergänzen."
   },
   "en": {
     "topics": "Topics",
     "interestedParties": "interested Parties",
-    "linkTitle": "Show {{title}}"
+    "linkTitle": "Show {{title}}",
+    "defaultDescription": "There is no description for this entry yet. You could add them in [GitHub](https://github.com/jschirrmacher/netvis/blob/master/example/data.json)."
   }
 }
 const texts = allTexts[lang] || allTexts[lang.substr(0, 2)] || allTexts[defaultLang]
