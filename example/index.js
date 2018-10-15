43892d15d04202f9378907fb73d97446de130ddb
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-10-15 22:37:23 +0200

    Can read YAML and from (YAML or JSON) files in a folder

--- example/index.js
+++ example/index.js
@@ -2,17 +2,39 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const DataCollector = require('./dataCollector')
+const dataCollector = new DataCollector()
+
+const PORT = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path}`)   // eslint-disable-line
+    console.log(`${req.method} ${req.path}`)   // eslint-disable-line no-console
     next()
 })
+
+function sendNodes(source, selector, res) {
+  const calculateFields = node => {
+    node.visible = !!selector.exec(node.type)
+    if (node.type === 'topic') {
+      node.fontSize = node.links ? Math.sqrt(node.links.reduce((s, e) => s + e.nodes.length, 0)) : 1
+    }
+    return node
+  }
+
+  dataCollector
+    .get(source)
+    .then(nodes => nodes.map(calculateFields))
+    .then(nodes => res.json({nodes}))
+}
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, '..', 'favicon.ico')))
 app.use('/dist', express.static(path.join(__dirname, '..', 'dist')))
+app.use('/data.yaml', (req, res) => sendNodes('data.yaml', /root|person/, res))
+app.use('/data.json', (req, res) => sendNodes('data.json', /topic/, res))
+app.use('/data', (req, res) => sendNodes('data', /root|person/, res))
 app.use(express.static(__dirname))
 
-app.listen(3000, () => {
-  console.log('Listening on http://localhost:3000') // eslint-disable-line
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`) // eslint-disable-line no-console
 })
